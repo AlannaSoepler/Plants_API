ea4ed89affa5f0bdfb85c3aba9b40ba31b87e0ae
AlannaSoepler <91547825+AlannaSoepler@users.noreply.github.com>
2022-10-26 13:14:39 +0100

    Swagger CRUD documentation

--- app/Http/Controllers/PlantController.php
+++ app/Http/Controllers/PlantController.php
@@ -12,8 +12,26 @@ use Illuminate\Http\Request;
 
 class PlantController extends Controller
 {
-    /**
+   /**
      * Display a listing of the resource.
+     *
+ * @OA\Get(
+ *     path="/api/plants",
+ *     description="Displays all the plants",
+ *     tags={"Plants"},
+     *      @OA\Response(
+        *          response=200,
+        *          description="Successful operation, Returns a list of Plants in JSON format"
+        *       ),
+        *      @OA\Response(
+        *          response=401,
+        *          description="Unauthenticated",
+        *      ),
+        *      @OA\Response(
+        *          response=403,
+        *          description="Forbidden"
+        *      )
+ * )
      *
      * @return \Illuminate\Http\Response
      */
@@ -24,6 +42,36 @@ class PlantController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     * * @OA\Post(
+     *      path="/api/plants",
+     *      operationId="store",
+     *      tags={"Plants"},
+     *      summary="Create a new Plant",
+     *      description="Stores the plant in the DB",
+     *      @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *            required={"name", "breed", "image", "info", "season", "environment", "hight", "provider", "available", "likes"},
+     *            @OA\Property(property="name", type="string", format="string", example="hibiscus"),
+     *            @OA\Property(property="breed", type="string", format="string", example="malvaceae"),
+     *            @OA\Property(property="image", type="string", format="string", example="https://xyz.com"),
+     *            @OA\Property(property="info", type="string", format="string", example="the hibiscus is a wonderful plant"),
+     *            @OA\Property(property="season", type="enum", format="enum", example="Me"),
+     *            @OA\Property(property="environment", type="string", format="string", example="Sunny"),
+     *            @OA\Property(property="hight", type="integer", format="integer", example="1"),
+     *            @OA\Property(property="provider", type="string", format="string", example="ThePlant.inc"),
+     *            @OA\Property(property="available", type="boolean", format="boolean", example="1"),
+     *             @OA\Property(property="likes", type="integer", format="integer", example="1")
+     *          )
+     *      ),
+     *     @OA\Response(
+     *          response=200, description="Success",
+     *          @OA\JsonContent(
+     *             @OA\Property(property="status", type="integer", example=""),
+     *             @OA\Property(property="data",type="object")
+     *          )
+     *     )
+     * )
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
@@ -38,7 +86,31 @@ class PlantController extends Controller
 
     /**
      * Display the specified resource.
-     *
+      * @OA\Get(
+    *     path="/api/plants/{id}",
+    *     description="Gets a plant by ID",
+    *     tags={"Plants"},
+    *          @OA\Parameter(
+        *          name="id",
+        *          description="Plant id",
+        *          required=true,
+        *          in="path",
+        *          @OA\Schema(
+        *              type="integer")
+     *          ),
+        *      @OA\Response(
+        *          response=200,
+        *          description="Successful operation"
+        *       ),
+        *      @OA\Response(
+        *          response=401,
+        *          description="Unauthenticated",
+        *      ),
+        *      @OA\Response(
+        *          response=403,
+        *          description="Forbidden"
+        *      )
+ * )
      * @param  \App\Models\Plant  $plant
      * @return \Illuminate\Http\PlantResource
      */
@@ -63,7 +135,26 @@ class PlantController extends Controller
         return new PlantResource($plant);
     }
 
-    /**
+     /**
+     * @OA\Delete(
+     *    path="/api/plants/{id}",
+     *    operationId="destroy",
+     *    tags={"Plants"},
+     *    summary="Delete a Plant",
+     *    description="Delete Plant",
+     *    @OA\Parameter(name="id", in="path", description="Id of a Plant", required=true,
+     *        @OA\Schema(type="integer")
+     *    ),
+     *    @OA\Response(
+     *         response=204,
+     *         description="Success",
+     *         @OA\JsonContent(
+     *         @OA\Property(property="status_code", type="integer", example="204"),
+     *         @OA\Property(property="data",type="object")
+     *          ),
+     *       )
+     *      )
+     *  )
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Plant  $plant
