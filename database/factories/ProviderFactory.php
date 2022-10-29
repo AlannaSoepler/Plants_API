1594711c60f8eb9ba941a1ae62cc03a4a28d9ca3
AlannaSoepler <91547825+AlannaSoepler@users.noreply.github.com>
2022-10-29 22:06:41 +0100

    Populating provider table

--- database/factories/ProviderFactory.php
+++ database/factories/ProviderFactory.php
@@ -17,7 +17,7 @@ class ProviderFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => fake()->name()
         ];
     }
 }
--- database/seeders/ProviderSeeder.php
+++ database/seeders/ProviderSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Provider;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,6 @@ class ProviderSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Provider::factory()->times(50)->create();
     }
 }
