c13cdbc9e35c6f55a8ae777cc1228b886a024b1d
AlannaSoepler <91547825+AlannaSoepler@users.noreply.github.com>
2022-10-31 14:30:17 +0000

    Show all providers

--- app/Http/Controllers/ProviderController.php
+++ app/Http/Controllers/ProviderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Provider;
 use Illuminate\Http\Request;
+use App\Http\Resources\ProviderCollection;
 
 class ProviderController extends Controller
 {
@@ -14,7 +15,7 @@ class ProviderController extends Controller
      */
     public function index()
     {
-        //
+        return new ProviderCollection(Provider::all());
     }
 
     /**
--- app/Http/Resources/ProviderResource.php
+++ app/Http/Resources/ProviderResource.php
@@ -14,6 +14,12 @@ class ProviderResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'logo' => $this->logo,
+            'info' => $this->info,
+            'email' => $this->email,
+            'telephone' => $this->telephone
+        ];
     }
 }
