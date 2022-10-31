d144f3f91e7420e94d30260decf25138372dca7e
AlannaSoepler <91547825+AlannaSoepler@users.noreply.github.com>
2022-10-31 18:22:34 +0000

    swagger get all providers

--- app/Http/Controllers/ProviderController.php
+++ app/Http/Controllers/ProviderController.php
@@ -9,8 +9,26 @@ use App\Http\Resources\ProviderCollection;
 
 class ProviderController extends Controller
 {
-    /**
+   /**
      * Display a listing of the resource.
+     *
+ * @OA\Get(
+ *     path="/api/providers",
+ *     description="Displays all the plants",
+ *     tags={"Providers"},
+     *      @OA\Response(
+        *          response=200,
+        *          description="Successful operation, Returns a list of Plants in JSON format"
+        *       ),
+        *      @OA\Response(
+        *          response=401,
+        *          description="Unauthenticated",
+        *      ),
+        *      @OA\Response(
+        *          response=403,
+        *          description="Forbidden"
+        *      )
+ * )
      *
      * @return \Illuminate\Http\Response
      */
