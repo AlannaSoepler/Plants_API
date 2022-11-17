492560c940885ca244878f2dec70bac90147dd73
AlannaSoepler <91547825+AlannaSoepler@users.noreply.github.com>
2022-11-17 11:53:26 +0000

    Created Auth control, changed routes plants & prov

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PlantController;
 use App\Http\Controllers\ProviderController;
 
@@ -17,6 +18,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/plants', PlantController::class);
-Route::apiResource('/providers', ProviderController::class);
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/auth/logout',[AuthController::class, 'logout']);
+    Route::get('/auth/user',[AuthController::class, 'user']);
+
+    Route::apiResource('/plants', PlantController::class)->except((['index', 'show']));
+    Route::apiResource('/providers', ProviderController::class)->except((['index', 'show']));
+});
+
+Route::get('/plants', [PlantController::class, 'index']);
+Route::get('/plants/{plant}', [PlantController::class, 'show']);
+
+Route::get('/providers', [ProviderController::class, 'index']);
+Route::get('/providers/{provider}', [ProviderController::class, 'show']);
 
